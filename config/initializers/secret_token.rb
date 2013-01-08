cb86b2bc51813953f56c83dd4f18e3854f35ebad
amy <amy@zendesk.com>
2013-01-09 00:36:42 +0800

    Initial Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webtest::Application.config.secret_token = 'cb1364d1415a055230bcba3e5076010eb97f7f9821a771922e29d80c141d14485c9ff021f42befda57c65a37409f6cd2d4c7a06e69f0122dc028267f4037b2b1'
